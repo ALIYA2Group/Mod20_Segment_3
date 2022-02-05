1129d3ea255a0268c8e9fc25600cae158e5c136a
seafishleo <seafishleo@gmail.com>
2022-02-05 14:46:43 -0500

    webpage update

--- Websites/main.py
+++ Websites/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 import py.scraping_idc
 
-app = Flask(__name__, static_folder = "web", static_url_path="", template_folder='templates')
+app = Flask(__name__, static_folder = "web", static_url_path="", template_folder='web')
 
 
 #set up app route
